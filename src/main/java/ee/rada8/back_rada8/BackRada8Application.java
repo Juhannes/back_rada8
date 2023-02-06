aec619c77b6904533b9e3a714d2e45f1393b0a43
Juhannes <pstimees@gmail.com>
2023-02-06 16:17:32 +0200

    Initial commit

---
+++ src/main/java/ee/rada8/back_rada8/BackRada8Application.java
@@ -0,0 +1,13 @@
+package ee.rada8.back_rada8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRada8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackRada8Application.class, args);
+	}
+
+}
---
+++ src/test/java/ee/rada8/back_rada8/BackRada8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.rada8.back_rada8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRada8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
