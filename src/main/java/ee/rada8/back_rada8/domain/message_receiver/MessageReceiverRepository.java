069604b1bb6731123c4847f7275e5ceb39532071
SILVER <kurriksilver@gmail.com>
2023-02-20 13:17:28 +0200

    Finishing up

--- src/main/java/ee/rada8/back_rada8/domain/message_receiver/MessageReceiverRepository.java
+++ src/main/java/ee/rada8/back_rada8/domain/message_receiver/MessageReceiverRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface MessageReceiverRepository extends JpaRepository<MessageReceiver, Integer> {
-    @Query("select m from MessageReceiver m where m.receiver.id = ?1 order by m.conversation.id")
-    List<MessageReceiver> findMessageReceiverEntries(Integer receiverId);
+//    @Query("select m from MessageReceiver m where m.receiver.id = ?1 order by m.conversation.id")
+//    List<MessageReceiver> findMessageReceiverEntries(Integer receiverId);
+
+    @Query("select m from MessageReceiver m where m.sender.id = ?1 or m.receiver.id = ?1 order by m.message.datetime DESC, m.conversation.id DESC")
+    List<MessageReceiver> findMessageReceiverEntries(Integer userId);
 
     @Query("select m from MessageReceiver m where m.conversation.id = ?1 and m.sender.id = ?2")
     List<MessageReceiver> findConversationBy(Integer conversationId, Integer senderId);
